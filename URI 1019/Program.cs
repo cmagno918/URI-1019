86430ef4b239acf4628bcfe5eb79e78a10029fb4
magno918 <cmagno918@gmail.com>
2021-09-04 15:36:11 -0300

    Adicionar arquivos de projeto.

---
+++ URI 1019/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace URI_1019
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N, segundos, minutos, horas, sobra;
+
+            N = int.Parse(Console.ReadLine());
+
+            horas = N / 3600;
+            sobra = N % 3600;
+
+            minutos = sobra / 60;
+            segundos = sobra % 60;
+
+            Console.WriteLine(horas + ":" + minutos + ":" + segundos );
+            
+        }
+    }
+}
